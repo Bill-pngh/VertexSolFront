5c63f406dff230d504bcf6125f57a78944b24444
Bill-pngh <grabillrose078@gmail.com>
2025-04-03 22:25:31 +0100

    ConnectWallet.js

---
+++ frontend/components/ConnectWallet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ConnectWallet = () => {
+  return (
+    <button className="bg-purple-500 px-4 py-2 rounded-lg mb-4">
+      🔑 Connect Wallet
+    </button>
+  );
+};
+
+export default ConnectWallet;
