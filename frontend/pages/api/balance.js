fb9acb5915b912aa516cae666e2ecaf2fc659142
Bill-pngh <grabillrose078@gmail.com>
2025-04-03 22:29:59 +0100

    balance.js

---
+++ frontend/pages/api/balance.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Features = () => {
+  return (
+    <button className="bg-gray-500 px-4 py-2 rounded-lg mb-4">
+      ⚙️ Features
+    </button>
+  );
+};
+
+export default Features;
