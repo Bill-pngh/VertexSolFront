f9ac0bd411a3b5f8aed6ff495a7cea39b012463e
Bill-pngh <grabillrose078@gmail.com>
2025-04-03 22:26:43 +0100

    Balance.js

---
+++ frontend/components/Balance.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Balance = () => {
+  return (
+    <button className="bg-blue-500 px-4 py-2 rounded-lg mb-4">
+      💰 Balance
+    </button>
+  );
+};
+
+export default Balance;
