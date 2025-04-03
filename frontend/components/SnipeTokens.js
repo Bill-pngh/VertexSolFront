9684b7e5f8b7f543a4b6c925c152710f796acb7e
Bill-pngh <grabillrose078@gmail.com>
2025-04-03 22:23:32 +0100

    SnipeTokens.js

---
+++ frontend/components/SnipeTokens.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SnipeTokens = () => {
+  return (
+    <button className="bg-green-500 px-4 py-2 rounded-lg mb-4">
+      🚀 Snipe New Tokens
+    </button>
+  );
+};
+
+export default SnipeTokens;
