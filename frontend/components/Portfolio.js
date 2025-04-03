36319662e280344394a2bc9706b7590ad09a0643
Bill-pngh <grabillrose078@gmail.com>
2025-04-03 22:24:55 +0100

    Portfolio.js

---
+++ frontend/components/Portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Portfolio = () => {
+  return (
+    <button className="bg-yellow-500 px-4 py-2 rounded-lg mb-4">
+      📊 Portfolio
+    </button>
+  );
+};
+
+export default Portfolio;
