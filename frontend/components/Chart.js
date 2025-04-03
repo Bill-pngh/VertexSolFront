927e09c3672d8e7a3ef5bbe607ad0f63cca3f115
Bill-pngh <grabillrose078@gmail.com>
2025-04-03 22:26:07 +0100

    Chart.js

---
+++ frontend/components/Chart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Chart = () => {
+  return (
+    <button className="bg-red-500 px-4 py-2 rounded-lg mb-4">
+      📈 Chart
+    </button>
+  );
+};
+
+export default Chart;
